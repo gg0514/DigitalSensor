06c4e64ec02dc9411b39ea799e3b8b0c8eebc8c4
pcm <pcm@bluesen.com>
2025-04-30 17:23:51 +0900

    Calib_1PSampleView UI 기능 추가

--- src/DigitalSensor/DigitalSensor/Views/Calib_2PBufferView.axaml.cs
+++ src/DigitalSensor/DigitalSensor/Views/Calib_2PBufferView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using DigitalSensor.ViewModels;
 
 namespace DigitalSensor.Views;
 
@@ -10,6 +11,12 @@ public partial class Calib_2PBufferView : UserControl
 
         // BreadcrumbBar를 사용하면, 메뉴이동 기능도 같이 만들어야 한다.
         //BreadcrumbBar1.ItemsSource = new string[] { "Setting", "Comm", };
-
+        this.AttachedToVisualTree += (_, _) =>
+        {
+            if (DataContext is Calib_2PBufferViewModel vm)
+            {
+                //vm.OnViewLoaded(); // ViewModel에서 정의한 메서드 호출
+            }
+        };
     }
 }
