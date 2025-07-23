9a70eaa795c9f9ad614bfb87b910872743c8fb59
pcm <pcm@bluesen.com>
2025-07-23 18:51:44 +0900

    Avalonia Custom 폰트 적용 성공

--- src/DigitalSensor/DigitalSensor.Android/MainActivity.cs
+++ src/DigitalSensor/DigitalSensor.Android/MainActivity.cs
@@ -1,5 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Android.Graphics;
 
 using Avalonia;
 using Avalonia.Android;
@@ -24,6 +27,21 @@ public class MainActivity : AvaloniaMainActivity<App>
 
         return base.CustomizeAppBuilder(builder)
             .WithInterFont();
+    }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // 시스템 UI 색상 변경
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+        {
+            Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+
+            Window.SetStatusBarColor(Color.Transparent);
+            Window.SetNavigationBarColor(Color.Transparent);
+        }
     }
 
     private void OnRegisterPlatformService(IServiceCollection services)
